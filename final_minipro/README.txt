74733b3b4e6043605c08e2222bbdd580e88ec119
amalvishnunk <37670313+amalvishnunk@users.noreply.github.com>
2018-12-04 22:28:58 +0530

    final commit(working)

---
+++ final_minipro/README.txt
@@ -0,0 +1,6 @@
+﻿npm install ejs --save
+npm install body-parser --save
+npm install express --save
+npm install mongoose --save
+
+
---
+++ final_minipro/src/model/transaction_data.js
@@ -0,0 +1,13 @@
+const mongoose=require('mongoose');
+mongoose.connect("mongodb://localhost:2017/BookBarterDB");
+const Schema=mongoose.Schema;
+var tran_schema=new Schema({
+    user_id:String,
+    book_id:String,
+    book_name:String,
+    status:String
+
+});
+
+var transaction=mongoose.model('transaction',tran_schema); 
+module.exports=transaction;
